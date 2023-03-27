f3e4f4db431dad718ee8f2656858dec3d71e95c6
Leiner Hoyos Cabrera <107592704+leinerdavidhc@users.noreply.github.com>
2023-03-27 12:49:42 -0500

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -2,3 +2,9 @@ SELECT c.nombre, v.fecha, v.total
 FROM clientes c
 INNER JOIN ventas v
 ON c.id = v.clientes_id;
+
+
+select clientes.nombre, ventas.clientes_id, ventas.total, ventas.fecha
+from clientes
+inner join ventas on (clientes.id = ventas.clientes_id)
+where ventas.fecha between '2023-03-23' and '2023-03-25'
